d1d4914b16b56fb54e6a15658d84cbf50976e656
Dan Bechard <dbechrd@gmail.com>
2020-02-26 00:04:02 -0800

    Clean up material

--- src/ta_material.c
+++ src/ta_material.c
@@ -1,5 +1,4 @@
 #include "ta_material.h"
-#include "ta_symbol.h"
 
 void ta_material_init(ta_material *material)
 {
@@ -23,13 +22,4 @@ void ta_material_init(ta_material *material)
     if (material->height_texture && !material->height_factor) {
         material->height_factor = 0.02f;
     }
-    //if (!material->albedo_texture)    { material->albedo_texture    = SYM_MISSING_ALBEDO; }
-    //if (!material->emission_texture)  { material->emission_texture  = SYM_MISSING_EMISSION; }
-    //if (!material->metallic_texture)  { material->metallic_texture  = SYM_MISSING_METALLIC; }
-    //if (!material->metallic_factor)   { material->metallic_factor   = 1.0f; }
-    //if (!material->roughness_texture) { material->roughness_texture = SYM_MISSING_ROUGHNESS; }
-    //if (!material->roughness_factor)  { material->roughness_factor  = 1.0f; }
-    //if (!material->height_texture)    { material->height_texture    = SYM_MISSING_HEIGHT; }
-    //if (!material->normal_texture)    { material->normal_texture    = SYM_MISSING_NORMAL; }
-    //if (!material->occlusion_texture) { material->occlusion_texture = SYM_MISSING_OCCLUSION; }
 }
